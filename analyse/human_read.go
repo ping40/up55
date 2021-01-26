916d2de4cf531a454649fccf8837d9f87ca61c5c
ping40 <norberthu30@gmail.com>
2021-01-26 17:56:20 +0800

    analyse 04

--- analyse/human_read.go
+++ analyse/human_read.go
@@ -8,7 +8,8 @@ import (
 )
 
 const (
-	zzc = "资产总计(万元)"
+	ZZC  = "资产总计(万元)"
+	ZJLR = "净利润(万元)"
 )
 
 func genHumanRead(code string, zcfzbResult, lrbResult, xjllbResult [][]string) {
@@ -28,11 +29,20 @@ func genHumanRead(code string, zcfzbResult, lrbResult, xjllbResult [][]string) {
 
 	//第二行 总资产
 	myC = NewColumn()
-	f.SetCellValue("Sheet1", myC.String(2), zzc)
-	rIndex := getRowIndex(zzc, zcfzbResult)
-
+	f.SetCellValue("Sheet1", myC.String(2), ZZC)
+	rIndex := getRowIndex(ZZC, zcfzbResult)
+	zzcList := zcfzbResult[rIndex]
 	for i := 0; i < columns; i++ {
-		f.SetCellValue("Sheet1", myC.String(2), zcfzbResult[rIndex][i+1])
+		f.SetCellValue("Sheet1", myC.String(2), zzcList[i+1])
+	}
+
+	//第三行 总净利润
+	myC = NewColumn()
+	f.SetCellValue("Sheet1", myC.String(3), ZJLR)
+	rIndex = getRowIndex(ZJLR, lrbResult)
+	zjlrList := lrbResult[rIndex]
+	for i := 0; i < columns; i++ {
+		f.SetCellValue("Sheet1", myC.String(3), zjlrList[i+1])
 	}
 
 	// Set active sheet of the workbook.
@@ -45,6 +55,7 @@ func genHumanRead(code string, zcfzbResult, lrbResult, xjllbResult [][]string) {
 }
 
 func getRowIndex(columnKey string, result [][]string) int {
+	fmt.Println("getRowIndex \n ")
 	for i := 0; i < len(result); i++ {
 		fmt.Println("all key:", result[i][0])
 		if result[i][0] == columnKey {
